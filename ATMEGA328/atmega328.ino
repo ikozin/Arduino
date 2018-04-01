535a127044e9c9770de67febd8be7493aa578132
Ilya Kozin <ikozin@yandex.ru>
2018-04-01 16:53:11 +0300

    update

--- ATMEGA328/atmega328.ino
+++ ATMEGA328/atmega328.ino
@@ -1,4 +1,21 @@
 /*
+http://codius.ru/articles/Arduino_UNO_4%D1%80%D0%B0%D0%B7%D1%80%D1%8F%D0%B4%D0%BD%D1%8B%D0%B9_7%D1%81%D0%B5%D0%B3%D0%BC%D0%B5%D0%BD%D1%82%D0%BD%D1%8B%D0%B9_%D0%B8%D0%B4%D0%B8%D0%BA%D0%B0%D1%82%D0%BE%D1%80_12_pin_3641BS_red
+https://masterkit.ru/images/sets_scheme/nm7039box_sx.jpg
+https://ecs7.tokopedia.net/img/product-1/2016/11/17/2550072/2550072_fa6ef4d0-76c7-4839-b123-9a678cd10d10.jpg
+       
+       4-разрядный 7-сегментный индикатор с общим анодом
+            1  A  F   2  3  B
+ ___________|__|__|___|__|__|____________
+|         |         |         |         |
+|    A    |    A    |    A    |    A    |
+| F     B | F     B | F     B | F     B |
+|    G    |    G    |    G    |    G    |
+| E     C | E     C | E     C | E     C |
+|    D    |    D    |    D    |    D    |
+|_______dp|_______dp|_______dp|_______dp|
+            |  |  |   |  |  |
+            E  D dp   C  G  4
+
                      ------
       RESET - PC6 --| *    |-- PC5 -  SCL  - A5
  0 -   RXD  - PD0 --|      |-- PC4 -  SDA  - A4
@@ -42,12 +59,11 @@ const int digitPinLength = sizeof(digitPin)/sizeof(digitPin[0]);
 
 void setup()
 {
-  //Serial.begin(9600); Нельзя так как pin 0 и 1 используются
+  Serial.begin(9600);
   timer = 0;
   
   DDRB = B11111111;
   PORTB = B11111111;
-
   for (int i = 0; i < digitPinLength; i++)
   {
     pinMode(digitPin[i], OUTPUT);
