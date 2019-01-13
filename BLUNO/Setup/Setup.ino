4d971bf93cb51587b4d7b9b2f9607d090d10bab9
Ilya Kozin <ikozin@yandex.ru>
2019-01-13 14:58:37 +0300

    bluno

---
+++ BLUNO/Setup/Setup.ino
@@ -0,0 +1,10 @@
+void setup() {
+  Serial.begin(115200);  //initial the Serial
+}
+
+void loop() {
+  if (Serial.available())  {
+    Serial.write(Serial.read());//send what has been received
+    Serial.println();   //print line feed character
+  }
+}
