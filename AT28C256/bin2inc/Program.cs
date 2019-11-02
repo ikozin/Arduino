0fca05b332d82ce7b743c760fcd47ab1acda9631
Ilya Kozin <ikozin@yandex.ru>
2019-11-02 16:06:48 +0300

    update

---
+++ AT28C256/bin2inc/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bin2inc
+{
+    class Program
+    {
+        private const int MaxLineCount = 16;
+        static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                displayHelp();
+                return;
+            }
+            var arrayName = args[0];
+            var binFile = args[1];
+            var incFile = args[2];
+            try
+            {
+                StringBuilder text = new StringBuilder(32768);
+                var data = File.ReadAllBytes(binFile);
+                text.AppendLine(String.Format("const uint8_t {0}[{1}]  PROGMEM =", arrayName, data.Length));
+                text.AppendLine("{");
+                for (int i = 0; i < data.Length; i++)
+                {
+                    text.AppendFormat("0x{0:X2}, ", data[i]);
+                    if (i % MaxLineCount == MaxLineCount - 1)
+                    {
+                        text.AppendLine();
+                    }
+                }
+                text.AppendLine("};");
+                File.WriteAllText(incFile, text.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void displayHelp()
+        {
+
+        }
+    }
+}
---
+++ AT28C256/bin2inc/run.cmd
@@ -0,0 +1,3 @@
+bin\Debug\bin2inc ram_test L:\Repos\ORION-128\rom\RAM_TEST.BIN     ..\AT28C256_Writer\ram_test.h
+bin\Debug\bin2inc m1       L:\Repos\ORION-128\rom\Orion128_M1.rom  ..\AT28C256_Writer\m1.h
+bin\Debug\bin2inc m2       L:\Repos\ORION-128\rom\Orion128_M2.rom  ..\AT28C256_Writer\m2.h
