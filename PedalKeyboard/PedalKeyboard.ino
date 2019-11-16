df92506a03271094c043905d870c1b15027412be
Ilya Kozin <ikozin@yandex.ru>
2019-11-16 16:16:00 +0300

    update

---
+++ PedalKeyboard/PedalKeyboard.ino
@@ -0,0 +1,14 @@
+#include <Keyboard.h>
+
+void setup()
+{
+	// initialize control over the keyboard:
+	Keyboard.begin();
+}
+
+void loop()
+{
+	delay(5000);
+	//Keyboard.print("Hello World!");
+	while (1) {}
+}
