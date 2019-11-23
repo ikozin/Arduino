6e2a33a841e24a7a6fc9db986c27743fc394ffff
Ilya Kozin <ikozin@yandex.ru>
2019-11-23 17:35:23 +0300

    pedal

--- PedalKeyboard/PedalKeyboard.ino
+++ PedalKeyboard/PedalKeyboard.ino
@@ -39,19 +39,61 @@ void loop()
 */
 
 #include <Keyboard.h>
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] =
+{
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] =
+{
+  4, 8, 8, 4, 4, 4, 4, 4
+};
 
 void setup()
 {
   // initialize control over the keyboard:
-  //Keyboard.begin();
+  Keyboard.begin();
   Serial.begin(9600);
+
+  for (int thisNote = 0; thisNote < 8; thisNote++)
+  {
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(5, melody[thisNote], noteDuration);
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(5);
+  }
+  
+}
+
+void DoAction()
+{
+  Serial.println("DoAction");
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('s');
+  delay(10);
+  Keyboard.releaseAll();  
 }
 
 void loop()
 {
-  delay(1000);
-  int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
+  delay(100);
+  int sensorValue0 = analogRead(A0);
+  if (sensorValue0 >= 200)
+  {
+    Serial.print("A0 = ");
+    Serial.println(sensorValue0);
+    DoAction();
+  }
   
 /*
   //Keyboard.print("Hello World!");
