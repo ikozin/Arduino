a85ae67ff0c3676990c93eb37d6af2e125218262
Ilya Kozin <ikozin@yandex.ru>
2021-02-27 20:18:08 +0300

    update

--- RDA5807M/RDA5807M.ino
+++ RDA5807M/RDA5807M.ino
@@ -1,25 +1,9 @@
 #include <Arduino.h>
-//#include <EEPROM.h>
 #include <Wire.h>
+#include <RotaryEncoder.h>    // https://github.com/mathertel/RotaryEncoder
 #include "radio.h"
-//#include <radio.h>
-//#include <RDA5807M.h>         // https://github.com/mathertel/Radio
-//#include <RotaryEncoder.h>    // https://github.com/mathertel/RotaryEncoder
 
-/*
-Скетч использует 2628 байт (8%) памяти устройства. Всего доступно 32256 байт.
-Глобальные переменные используют 297 байт (14%) динамической памяти, оставляя 1751 байт для локальных переменных. Максимум: 2048 байт.
-
-Скетч использует 4004 байт (12%) памяти устройства. Всего доступно 32256 байт.
-Глобальные переменные используют 400 байт (19%) динамической памяти, оставляя 1648 байт для локальных переменных. Максимум: 2048 байт.
-
-Скетч использует 4372 байт (13%) памяти устройства. Всего доступно 32256 байт.
-Глобальные переменные используют 430 байт (20%) динамической памяти, оставляя 1618 байт для локальных переменных. Максимум: 2048 байт.
-
-Скетч использует 4426 байт (13%) памяти устройства. Всего доступно 32256 байт.
-Глобальные переменные используют 404 байт (19%) динамической памяти, оставляя 1644 байт для локальных переменных. Максимум: 2048 байт.
-
-*/
+#define DEBUG
 
 //ARDUINO_AVR_MINI
 //ARDUINO_AVR_NANO
@@ -28,13 +12,17 @@
 #if !defined(ARDUINO_AVR_UNO)
   #error Select ARDUINO_AVR_UNO
 #endif
+//dt clk
+//s2 s1
+RotaryEncoder encoderVolume(2, 3, RotaryEncoder::LatchMode::FOUR3);
+//RDA5807M radio;
 
-uint8_t currentVolume = 2;    // 0..15
-uint16_t currentBand = 1017;  // 101.7FM
+uint8_t currentVolume = 3;    // 0..15
+uint8_t currentIndex = 35;
 
 typedef struct _radioItem {
   uint16_t band;
-  char name[30];
+  char name[62];
 } RadioItem_t;
 
 const RadioItem_t radioList[] PROGMEM = {
@@ -74,13 +62,13 @@ const RadioItem_t radioList[] PROGMEM = {
   { 1009, "Радио Вера" },
   { 1012, "Радио DFM" },
   { 1017, "Наше Радио" },
-  { 1021, "Радио Монте-Карло" },
+  { 1021, "Радио Монте-Карлo" },
   { 1025, "Comedy FM" },
   { 1030, "Радио Шансон" },
   { 1034, "Радио Маяк" },
   { 1037, "Радио Максимум" },
   { 1042, "Радио Энергия FM" },
-  { 1047, "Радио 7 На Семи Холмах" },
+  { 1047, "Радио 7 На Семи Хoлмах" },
   { 1050, "Радио Книга" },
   { 1053, "Capital FM" },
   { 1057, "Русское Радио" },
@@ -93,163 +81,175 @@ const RadioItem_t radioList[] PROGMEM = {
 
 const int listSize = sizeof(radioList) / sizeof(RadioItem_t);
 
-uint8_t currentIndex = 35;
-
-/*
-RDA5807M radio;
-
-RotaryEncoder encoderVolume(2, 3, RotaryEncoder::LatchMode::FOUR3);
-
-int currentVolume = 2;
-int currentBand = 10170;
-int currentIndex = 0;   // индекс радиостанции
-*/
-
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  Serial.println();
+  Serial.println();
+
   
   radioInit();
-  //radioSetBand(currentBand);
-  selectStation(currentIndex);
+  radioSetRadio(currentIndex);
   radioSetVolume(currentVolume);
+  
+  //radioInit();
+  //radioSetVolume(currentVolume);
+  //selectStation(currentIndex);
+  //radioSetHardMute(false);
+  //radioSetMute(false);
     
-/*
+
   pinMode(4, INPUT_PULLUP);
 
-  radio.init();
-  //radio.debugEnable();
-  //setBand(currentBand);
-  setRadio(currentIndex);
-  setVolume(currentVolume);
-  setMono(false);
-  setMute(false);
-*/
+  //radio.init();
+  //radio.setBand(RADIO_BAND_FM);
+  ////radio.debugEnable();
+  ////setBand(currentBand);
+  //setRadio(currentIndex);
+  //radio.setVolume(currentVolume);
+  //radio.setMono(false);
+  //radio.setMute(false);
 }
 
 void loop() {
-  uint8_t RSSI = radioGetRssi();
-  Serial.print(F("RSSI = "));
-  Serial.print(RSSI);
-  Serial.println(F(" (0-min, 127-max)"));
-  delay(500);
-  
-/*
-  encoderVolume.tick();
+  //uint8_t RSSI = radioGetRssi();
+  //Serial.print(F("RSSI = "));
+  //Serial.print(RSSI);
+  //Serial.println(F(" (0-min, 127-max)"));
+  //delay(500);
+  //Serial.println(radioGetHardMute());
 
+ 
+  encoderVolume.tick();
   if (digitalRead(4) == LOW) {
-    setMute(!radio.getMute());
+    radioSetMute(!radioGetMute());
     delay(1000);
   }
-  
   int dir = (int)encoderVolume.getDirection();
+  handleChannel(dir);
+}
+
+void handleChannel(int dir) {
   if (dir > 0) {
-    if (currentIndex < listSize) {
-      setRadio(++currentIndex);
+    if (currentIndex < listSize - 1) {
+      radioSetRadio(++currentIndex);
     }
   }
   if (dir < 0) {
     if (currentIndex > 0) {
-      setRadio(--currentIndex);
+      radioSetRadio(--currentIndex);
     }
   }
-*/
+}
 
-/*
+void handleVolume(int dir) {
   if (dir > 0) {
     if (currentVolume < 15) {
-      setVolume(++currentVolume);
+      radioSetVolume(++currentVolume);
     }
   }
   if (dir < 0) {
     if (currentVolume > 0) {
-      setVolume(--currentVolume);
+      radioSetVolume(--currentVolume);
     }
   }
-*/
-}
-/*
-void setMute(bool value) {
-  radio.setMute(value);
-  Serial.print("Mute: ");
-  Serial.println(value);  
 }
 
-void setMono(bool value) {
-  radio.setMono(value);
-  Serial.print("Mono: ");
-  Serial.println(value);  
-}
-
-void setBand(int value) {
-  radio.setBandFrequency(RADIO_BAND_FM, value);
-  Serial.print("Band: ");
-  Serial.println(value);  
-}
-
-void setVolume(int volume) {
-  radio.setVolume(volume);
-  Serial.print("Volume: ");
-  Serial.println(volume);  
-}
-
-void setRadio(int value) {
-  RadioItem_t row = radioList[value];
-  radio.setBandFrequency(RADIO_BAND_FM, row.band);
-  Serial.print("Band: ");
-  Serial.print(row.band);
-  Serial.print(" , Name: ");
-  Serial.println(row.name);  
-}
-*/
-
-void selectStation(uint8_t index) {
+void radioSetRadio(uint8_t index) {
   char c;
-  byte *p_item = (byte *)(radioList + index);
-  uint16_t band = (uint16_t)pgm_read_dword_near(p_item);
-  radioSetBand(band);
-
-  p_item += sizeof(long);
-  while ((c = (char)pgm_read_byte_near(p_item)) != '\0') {
+  byte *pData = (byte *)(radioList + index);
+  Serial.print(' ');
+  uint16_t band = pgm_read_word(pData);
+  Serial.print(band);
+  Serial.print(':');
+  pData += sizeof(uint16_t);
+  while ((c = (char)pgm_read_byte(pData++)) != 0) {
     Serial.print(c);
-    p_item++;
   }
   Serial.println();
+  radioSetChannel(band);
 }
 
 void radioInit() {
-  uint16_t reg02h = RDA5807M_FLG_ENABLE | RDA5807M_FLG_DHIZ | RDA5807M_FLG_DMUTE | RDA5807M_FLG_BASS;
-  setRegister(RDA5807M_REG_CONFIG, reg02h);
+  rda_reg2_t reg2 = { .value = 0 };
+  reg2.DMUTE = 1;
+  reg2.DHIZ = 1;
+  reg2.ENABLE = 1;
+  reg2.BASS = 1;
+  setRegister(RDA5807M_REG2, reg2.value);
+  
+  rda_reg5_t reg5 = { .value = 0 };
+  reg5.LNA_PORT_SEL = 2;
+  reg5.SEEKTH = 8;
+  setRegister(RDA5807M_REG5, reg5.value);
+  delay(1000);
 }
 
-void radioSetBand(uint16_t value) {
-  uint16_t reg03h = (value - 870) << RDA5807M_CHAN_SHIFT;
-  setRegister(RDA5807M_REG_TUNING, reg03h | RDA5807M_FLG_TUNE);  
+void radioSetChannel(uint16_t value) {
+  rda_reg3_t reg = { .value = 0 };
+  reg.CHAN = value - RADIO_BAND_MIN;
+  reg.TUNE = 1;
+  reg.BAND = RADIO_BAND_WIDTH; //00 - 87..108МГц, 01 - 76..91МГц, 10 - 76..108МГц, 11 - 65..76МГц или 50..65МГц (определяется битом 65M_50M MODE регистра 07h)
+  setRegister(RDA5807M_REG3, reg.value);
+  waitTune();
+  delay(200);
 }
 
-void radioSetVolume(uint16_t value) {
-  uint16_t reg05h = getRegister(RDA5807M_REG_VOLUME);
-  reg05h &= ~RDA5807M_VOLUME_MASK;
-  reg05h |= value << RDA5807M_VOLUME_SHIFT;
-  setRegister(RDA5807M_REG_VOLUME, reg05h);
+uint16_t radioGetChannel() {
+  rda_reg3_t reg = { .value = getRegister(RDA5807M_REG3) };
+  return reg.CHAN + RADIO_BAND_MIN;
 }
 
-bool radioGetMute() {
-  uint16_t reg02h = getRegister(RDA5807M_REG_CONFIG);
-  return (reg02h & RDA5807M_FLG_DMUTE) == 0;
+void radioSetVolume(uint8_t value) {
+#ifdef DEBUG
+  Serial.print("Volume = ");
+  Serial.println(value);
+#endif
+  rda_reg5_t reg = { .value = getRegister(RDA5807M_REG5) };
+  reg.VOLUME = value;
+  setRegister(RDA5807M_REG5, reg.value);
+  delay(100);
+}
+
+uint8_t radioGetVolume() {
+  rda_reg5_t reg = { .value = getRegister(RDA5807M_REG5) };
+  return reg.VOLUME;
 }
 
 void radioSetMute(bool value) {
-  uint16_t reg02h = getRegister(RDA5807M_REG_CONFIG);
-  reg02h |= RDA5807M_FLG_DMUTE;
-  if (value) reg02h &= ~RDA5807M_FLG_DMUTE;
-  setRegister(RDA5807M_REG_CONFIG, reg02h);
+  rda_reg2_t reg = { .value = getRegister(RDA5807M_REG2) };
+  reg.DMUTE = !value;
+  setRegister(RDA5807M_REG2, reg.value);
+}
+
+bool radioGetMute() {
+  rda_reg2_t reg = { .value = getRegister(RDA5807M_REG2) };
+  return reg.DMUTE == 0;
+}
+
+void radioSetHardMute(bool value) {
+  rda_reg2_t reg = { .value = getRegister(RDA5807M_REG2) };
+  reg.DHIZ = !value;
+  setRegister(RDA5807M_REG2, reg.value);
+}
+
+bool radioGetHardMute() {
+  rda_reg2_t reg = { .value = getRegister(RDA5807M_REG2) };
+  return reg.DHIZ == 0;
 }
 
 uint8_t radioGetRssi() {
-  uint16_t reg0Bh = getRegister(RDA5807M_REG_RSSI);
-  uint8_t RSSI = (reg0Bh & RDA5807M_RSSI_MASK) >> RDA5807M_RSSI_SHIFT;
-  return RSSI;  
+  rda_regb_t reg;
+  reg.value = getRegister(RDA5807M_REGB);
+  return reg.RSSI;  
+}
+
+
+void waitTune() {
+  rda_rega_t reg;
+  do {
+    reg.value = getRegister(RDA5807M_REGA);
+  } while (reg.STC == 0);
 }
 
 uint16_t getRegister(uint8_t reg) {
@@ -257,13 +257,19 @@ uint16_t getRegister(uint8_t reg) {
   Wire.beginTransmission(RDA5807M_RANDOM_ACCESS_ADDRESS);
   Wire.write(reg);
   Wire.endTransmission(false);
-  Wire.requestFrom(0x11, 2, true);
+  Wire.requestFrom(RDA5807M_RANDOM_ACCESS_ADDRESS, 2, true);
   result = (uint16_t)Wire.read() << 8;
   result |= Wire.read();
   return result;
 }
+
 void setRegister(uint8_t reg, const uint16_t value) {
-  Wire.beginTransmission(0x11);
+#ifdef DEBUG
+  char text[128];
+  sprintf(text, "0x%02X = 0x%04X", reg, value);
+  Serial.println(text);
+#endif
+  Wire.beginTransmission(RDA5807M_RANDOM_ACCESS_ADDRESS);
   Wire.write(reg);
   Wire.write(highByte(value));
   Wire.write(lowByte(value));
