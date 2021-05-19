8b1bfa2624cea00a7cccff166e84a02f5de870de
Ilya Kozin <ikozin@yandex.ru>
2021-05-19 18:44:15 +0300

    1) правка шаблона alarm.ttt 2) использование шаблона alarm.ttt и страницы alarm.html (убраны соответствующие строковые представления)

--- LilyGO-TTGO/T-Display/ESP32_I2C_Radio/webPage_Alarm.ino
+++ LilyGO-TTGO/T-Display/ESP32_I2C_Radio/webPage_Alarm.ino
@@ -1,97 +1,15 @@
 
-String alert_ttt = String(R"rawliteral(<li class='list-group-item'>
-        <form method='post'>
-          <div class='row'>
-            <div class='col-auto'>
-              <input type='time' class='form-control' name='time' value='%time%'>
-            </div>            
-            <div class='col-auto'>
-              <div class='form-check'>
-                <input class='form-check-input' type='checkbox' name='day1'%day1%/>
-                <label class='form-check-label' for='day1'>Понедельник</label>
-              </div>            
-              <div class='form-check'>
-                <input class='form-check-input' type='checkbox' name='day2'%day2%/>
-                <label class='form-check-label' for='day2'>Вторник</label>
-              </div>            
-              <div class='form-check'>
-                <input class='form-check-input' type='checkbox' name='day3'%day3%/>
-                <label class='form-check-label' for='day3'>Среда</label>
-              </div>            
-              <div class='form-check'>
-                <input class='form-check-input' type='checkbox' name='day4'%day4%/>
-                <label class='form-check-label' for='day4'>Четверг</label>
-              </div>            
-              <div class='form-check'>
-                <input class='form-check-input' type='checkbox' name='day5'%day5%/>
-                <label class='form-check-label' for='day5'>Пятница</label>
-              </div>            
-              <div class="form-check">
-                <input class='form-check-input' type='checkbox' name='day6'%day6%/>
-                <label class='form-check-label' for='day6'>Суббота</label>
-              </div>            
-              <div class='form-check'>
-                <input class='form-check-input' type='checkbox' name='day7'%day7%/>
-                <label class='form-check-label' for='day7'>Восресение</label>
-              </div>            
-            </div>            
-            <div class='col-auto'>
-              <button type='submit' class='btn btn-primary' name='edit' value='%index%'>Изменить</button>
-              <button type='submit' class='btn btn-primary' name='del' value='%index%'>Удалить</button>
-            </div>
-          </div>
-        </form>
-      </li>)rawliteral");
-
-
-
-String alert_html = String(R"rawliteral(
-<!DOCTYPE HTML>
-<html lang='ru'>
-<head>
-  <meta http-equiv='content-type' content='text/html; charset=utf-8'>
-  <meta name='viewport' content='width=device-width, initial-scale=1'>
-  <title>ESP32 TTGO T-Display</title>
-  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css'/>
-  <style>
-  </style>
-</head>
-<body>
-  <br/>
-  <div class='container'>
-    <div class='row'>
-      <form method="post">
-          <button type='submit' class='btn btn-primary' name='add'>Добавить новый</button>
-      </form>
-    </div>
-    <br/>
-    <ul class='list-group'>
-    %list%
-    </ul>
-    <br/>
-  </div>
-</body>
-</html>
-)rawliteral");
-
-
-String processorAlarm(const String& var) {
-  if (var == "list") {
-    String text = String();
-    for (int i = 0; i < settingsCount; i++) {
-      text.concat("\r\n<li class=\"list-group-item\">");
-      text.concat("\r\n<button type=\"submit\" class=\"btn btn-primary\" name=\"edit\" value=\"");
-      text.concat(i);
-      text.concat("\">Изменить</button>");
-      text.concat("\r\n<button type=\"submit\" class=\"btn btn-primary\" name=\"del\" value=\"");
-      text.concat(i);
-      text.concat("\">Удалить</button>");
-      text.concat("\r\n</li>");      
+void loadFile(const char* fileName, String& text) {
+  File f = SPIFFS.open(fileName);
+  if (f) {
+    size_t len = f.size();
+    if (len <= sizeof(fileData)) {
+      f.read((uint8_t*)fileData, len);
+      ((uint8_t*)fileData)[len] = 0;
+      text = (char*)fileData;
     }
-    text.concat("\r\n");      
-    return text;
+    f.close();
   }
-  return emptyString;
 }
 
 void pageAlarmGet(AsyncWebServerRequest *request) {
@@ -99,17 +17,23 @@ void pageAlarmGet(AsyncWebServerRequest *request) {
   debug_printf("GET: Alarm\r\n");
   logRequest(request);
 #endif
-
-  String alert = String(alert_ttt);
+  
+  String alert = String();
+  String templ = String();
   String list = String();
-  String page = String(alert_html);
+  String html = String();
   alert.reserve(2048);
+  templ.reserve(2048);
   list.reserve(16384);
-  page.reserve(16384);
+  html.reserve(16384);
+  loadFile("/alarm.ttt", templ);
+  loadFile("/alarm.html", html);
+  if (templ.length() == 0 || html.length() == 0) return page404(request);
+  
   for (int i = 0; i < settingsCount; i++) {
     alarm_t setting = settings[i];
-    String alert = String(alert_ttt);
-    
+
+    alert = String(templ);
     alert.replace("%index%", String(i));
     sprintf((char *)fileData, "%02d:%02d", setting.Hour, setting.Minute);
     String time = (char *)fileData;
@@ -123,11 +47,10 @@ void pageAlarmGet(AsyncWebServerRequest *request) {
     alert.replace("%day1%", setting.Monday     ? checkedString: emptyString);
 
     list.concat(alert);
+    alert.clear();
   }
-  page.replace("%list%", list);
-  //debug_printf(alert_html.c_str());
-  //request->send(SPIFFS, "/alarm.html", emptyString, false, processorAlarm);
-  request->send(200, "text/html", page);
+  html.replace("%list%", list);
+  request->send(200, "text/html", html);
 }
 
 void parseTime(alarm_t& setting, AsyncWebServerRequest *request) {
@@ -153,7 +76,6 @@ void pageAlarmPost(AsyncWebServerRequest *request) {
 #endif
   if (request->hasParam("add", true)) {
     if (settingsCount >= MaxSettingsCount) return request->redirect("/");
-
     // Вставляем в начало
     for (uint16_t i = MaxSettingsCount - 1; i > 0; i--) settings[i].value = settings[i - 1].value;
     settings[0].value = 0x0800007F;
@@ -163,7 +85,7 @@ void pageAlarmPost(AsyncWebServerRequest *request) {
   if (request->hasParam("del", true)) {
     uint16_t index = request->getParam("del", true)->value().toInt();
     if (settingsCount == 0 || index >= settingsCount)  return request->redirect("/");
-
+    // Сдвигаем на место удаленного
     for (uint16_t i = index; i < MaxSettingsCount - 1; i++) settings[i].value = settings[i + 1].value;
     settings[MaxSettingsCount - 1].value = 0;
     settingsCount--;
@@ -171,7 +93,7 @@ void pageAlarmPost(AsyncWebServerRequest *request) {
   }
   if (request->hasParam("edit", true)) {
     uint16_t index = request->getParam("edit", true)->value().toInt();
-    if (index >= settingsCount)  return request->redirect("/alarm.html");
+    if (index >= settingsCount)  return request->redirect("/");
 
     alarm_t& setting = settings[index];
     parseTime(setting, request);
