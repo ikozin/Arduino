3ca2b160bd26c6c29d3f03e356b1991cc5d00533
Ilya Kozin <ikozin@yandex.ru>
2021-07-17 14:08:08 +0300

    update

--- HD44780 LCD IIC SPI V2.0/demo/demo.ino
+++ HD44780 LCD IIC SPI V2.0/demo/demo.ino
@@ -1,10 +1,11 @@
 #include "LiquidCrystal.h"
 
-LiquidCrystal lcd(0);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal lcd(0);  // 0 = I2C LCD address 0x20, 1 = I2C LCD address 0x21
 
 void setup()
 {
-  lcd.begin(16,2);                      // initialize the lcd 
+  //lcd.begin(16,2);
+  lcd.begin(20,4);
 }
 
 
@@ -18,7 +19,8 @@ void loop()
   flipDisplay();
   flipBacklight();
   waitAnimation(3);
-  displayCharset();
+  //displayCharset16x2();
+  displayCharset20x4();
 }
 
 void flipDisplay() {
@@ -62,7 +64,7 @@ void waitAnimation(int count) {
   } 
 }
 
-void displayCharset() {
+void displayCharset16x2() {
   lcd.clear();
   lcd.setCursor(0,0); for (int i = 0x00; i <= 0x0F; i++) lcd.print((char)i);
   lcd.setCursor(0,1); for (int i = 0x10; i <= 0x1F; i++) lcd.print((char)i);
@@ -101,5 +103,35 @@ void displayCharset() {
   lcd.clear();
   lcd.setCursor(0,0); for (int i = 0xE0; i <= 0xEF; i++) lcd.print((char)i);
   lcd.setCursor(0,1); for (int i = 0xF0; i <= 0xFF; i++) lcd.print((char)i);
+  delay(2000);
+}
+
+void displayCharset20x4() {
+  lcd.clear();
+  lcd.setCursor(0,0); //lcd.print("00: "); /for (int i = 0x00; i <= 0x4F; i++) lcd.print((char)i);
+  lcd.setCursor(0,1); lcd.print("10: "); for (int i = 0x10; i <= 0x1F; i++) lcd.print((char)i);
+  lcd.setCursor(0,2); lcd.print("20: "); for (int i = 0x20; i <= 0x2F; i++) lcd.print((char)i);
+  lcd.setCursor(0,3); lcd.print("30: "); for (int i = 0x30; i <= 0x3F; i++) lcd.print((char)i);
+  delay(2000);
+
+  lcd.clear();
+  lcd.setCursor(0,0); lcd.print("40: "); for (int i = 0x40; i <= 0x4F; i++) lcd.print((char)i);
+  lcd.setCursor(0,1); lcd.print("50: "); for (int i = 0x50; i <= 0x5F; i++) lcd.print((char)i);
+  lcd.setCursor(0,2); lcd.print("60: "); for (int i = 0x60; i <= 0x6F; i++) lcd.print((char)i);
+  lcd.setCursor(0,3); lcd.print("70: "); for (int i = 0x70; i <= 0x7F; i++) lcd.print((char)i);
+  delay(2000);
+
+  lcd.clear();
+  lcd.setCursor(0,0); lcd.print("80: "); for (int i = 0x80; i <= 0x8F; i++) lcd.print((char)i);
+  lcd.setCursor(0,1); lcd.print("90: "); for (int i = 0x90; i <= 0x9F; i++) lcd.print((char)i);
+  lcd.setCursor(0,2); lcd.print("A0: "); for (int i = 0xA0; i <= 0xAF; i++) lcd.print((char)i);
+  lcd.setCursor(0,3); lcd.print("B0: "); for (int i = 0xB0; i <= 0xBF; i++) lcd.print((char)i);
+  delay(2000);
+
+  lcd.clear();
+  lcd.setCursor(0,0); lcd.print("C0: "); for (int i = 0xC0; i <= 0xCF; i++) lcd.print((char)i);
+  lcd.setCursor(0,1); lcd.print("D0: "); for (int i = 0xD0; i <= 0xDF; i++) lcd.print((char)i);
+  lcd.setCursor(0,2); lcd.print("E0: "); for (int i = 0xE0; i <= 0xEF; i++) lcd.print((char)i);
+  lcd.setCursor(0,3); lcd.print("F0: "); for (int i = 0xF0; i <= 0xFF; i++) lcd.print((char)i);
   delay(2000);
 }
