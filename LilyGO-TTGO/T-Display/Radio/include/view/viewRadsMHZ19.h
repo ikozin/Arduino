40d0ef9ba4601c7d282f228d567fdcfee26719de
Ilya Kozin <ikozin@yandex.ru>
2024-10-03 21:37:36 +0300

    mhzh19

--- LilyGO-TTGO/T-Display/Radio/include/view/viewRadsMHZ19.h
+++ LilyGO-TTGO/T-Display/Radio/include/view/viewRadsMHZ19.h
@@ -4,13 +4,23 @@
 #include "controller/controllerRadSens.h"
 #include "controller/controllerMHZ19.h"
 
-class ViewRadsMHZ19 : public View {
+typedef enum _Co2Level {
+    Min = 0,
+    Middle,
+    Max,
+} Co2Level;
+
+class ViewRadsMHZ19 : public ViewT {
     public:
         ViewRadsMHZ19(const char* name, ViewSettig* setting, ControllerRadSens* radSens, ControllerMHZ19* mhz19, SemaphoreHandle_t updateEvent = nullptr);
-        virtual void Start(uint16_t stackDepth = 2048) override;
     protected:
         virtual void OnDrawHandle() override;
     private:
-        ControllerRadSens*  _radSens;
+        uint32_t            _co2_Min;
+        uint32_t            _co2_Max;
         ControllerMHZ19*    _mhz19;
+    private:
+        Co2Level getCO2Level(float value) const {
+            return (value <= _co2_Min) ? Co2Level::Min : ((value > _co2_Max) ? Co2Level::Max : Co2Level::Middle);
+        };
 };
--- LilyGO-TTGO/T-Display/Radio/src/view/viewRadsMHZ19.cpp
+++ LilyGO-TTGO/T-Display/Radio/src/view/viewRadsMHZ19.cpp
@@ -3,87 +3,77 @@
 #include "fonts/Roboto33.h"
 
 ViewRadsMHZ19::ViewRadsMHZ19(const char* name, ViewSettig* setting,  ControllerRadSens* radSens, ControllerMHZ19* mhz19, SemaphoreHandle_t updateEvent):
-                View(name, setting, updateEvent) {
-    _radSens = radSens;
+                ViewT(name, setting, radSens, (uint64_t)50000, updateEvent) {
+    _co2_Min = 800;
+    _co2_Max = 1200;
     _mhz19 = mhz19;
 }
 
-void ViewRadsMHZ19::Start(uint16_t stackDepth){
-        if (_radSens != nullptr) {
-            _radSens->AddUpdateEvent(GetEvent());
-        }
-        if (_mhz19 != nullptr) {
-            _mhz19->AddUpdateEvent(GetEvent());
-        }
-        xTaskCreate(ViewHandler, this->_name, stackDepth, this, 100, &this->_task);
-}
-
-
+int counter = 0;
+int cnt = 0;
 void ViewRadsMHZ19::OnDrawHandle() {
     // LOGN("%s::OnDrawHandle", typeid(this).name);
+    ControllerRadSens* _radSens = static_cast<ControllerRadSens*>(_ctrl);
 
+    uint32_t backColor;
+    uint32_t foreColor;
     char text[8];
 
     float rad = 0;
     if (_radSens) {
-        rad = _radSens->getStatic();
+        rad = _radSens->getDynamic();
     }
-    int co2 = 400;
+    int co2 = 600;
     if (_mhz19) {
         co2 = _mhz19->getCO2();
     }
-
-    // uint32_t backColor = TFT_DARKGREY; //TFT_DARKGREEN;
-    // if (rad > 30) {
-    //     backColor = TFT_RED;
-    // } else if (rad > 20) {
-    //     backColor = TFT_YELLOW;
-    // }
-
-    uint32_t backColor = TFT_DARKCYAN;
+    co2 += cnt++;
+    if (co2 > 1400) {
+        cnt = 0;
+    }
+    
+    getSetting()->getSprite()->loadFont(RobotoBold_33);
     getSetting()->getSprite()->fillSprite(TFT_BLACK);
-    // getSetting()->getSprite()->drawSmoothRoundRect(5, 5, 15, 10, 112, 125, TFT_RED, backColor);
-
-
-    getSetting()->getSprite()->fillSmoothRoundRect(5, 5, 112, 125, 15, TFT_RED, backColor);
-    getSetting()->getSprite()->fillSmoothRoundRect(123, 5, 112, 125, 15, TFT_YELLOW, backColor);
-
-    // getSetting()->getSprite()->fillSmoothRoundRect(5, 5, 112, 125, 15, backColor);
-    // getSetting()->getSprite()->fillSmoothRoundRect(123, 5, 112, 125, 15, backColor);
-
-    uint16_t y2 = 67;
-    uint16_t x2 = 120;
-
-    //_sprite->drawFastVLine(x2, 0, TFT_WIDTH, TFT_WHITE);
-
-    getSetting()->getSprite()->setTextColor(TFT_WHITE);
     getSetting()->getSprite()->setTextDatum(CC_DATUM);
-    getSetting()->getSprite()->loadFont(Roboto_33);
-
-    sprintf(text, "%d", co2);
-    getSetting()->getSprite()->drawString(text, 60, 67);
-
+    // getSetting()->getSprite()->drawSmoothRoundRect(5, 5, 15, 10, 112, 125, TFT_RED, backColor);
+    
+    auto co2level = getCO2Level(co2);
+    switch (co2level) {
+        case Co2Level::Min:
+            foreColor = TFT_BLUE;
+            backColor = TFT_GREEN;
+            counter = 0;
+            break;
+        case Co2Level::Middle:
+            foreColor = TFT_BLACK;
+            backColor = TFT_YELLOW;
+            counter ++;
+            break;
+        case Co2Level::Max:
+            foreColor = TFT_BLACK;
+            backColor = TFT_RED;
+            counter ++;
+            break;
+    }
+    if (counter < 10) {
+        getSetting()->getSprite()->fillSmoothRoundRect(5, 5, 112, 125, 15, backColor, TFT_BLACK);
+        getSetting()->getSprite()->setTextColor(foreColor);
+        sprintf(text, "%d", co2);
+        getSetting()->getSprite()->drawString(text, 60, 67);
+    }
+    if (counter > 14) {
+        counter = 0;
+    }
+    foreColor = TFT_WHITE;
+    backColor = (rad <= 15) ? TFT_GREEN: TFT_RED;
+    foreColor = (rad <= 15) ? TFT_BLUE: TFT_BLACK;
+    getSetting()->getSprite()->fillSmoothRoundRect(123, 5, 112, 125, 15, backColor, TFT_BLACK);
+    getSetting()->getSprite()->setTextColor(foreColor);
     sprintf(text, "%.0f", rad);    //μR/h
     getSetting()->getSprite()->drawString(text, 180, 67);
 
-
-    // _sprite->unloadFont();
-
-    // _sprite->drawFloat(_bme280->getTemperature(), 0, x4, y4 + 7, 7);
-    // _sprite->drawFloat(_bme280->getHumidity(), 0, x4 + x2, y4 + 7, 7);
-    // _sprite->drawFloat(_bme280->getPressure(), 0, x4, y4 + y2 + 7, 7);
-    // _sprite->drawFloat(rad, 0, x4 + x2, y4 + y2 + 7, 7);
-
-    // _sprite->drawFastHLine(0, 0,             TFT_HEIGHT, TFT_WHITE);
-    // _sprite->drawFastHLine(0, y2,            TFT_HEIGHT, TFT_WHITE);
-    // _sprite->drawFastHLine(0, TFT_WIDTH - 1, TFT_HEIGHT, TFT_WHITE);
-
-
-    // _sprite->setTextDatum(TL_DATUM);    
-    // _sprite->fillRect(0,  0,  x2 - 36, 12, TFT_WHITE);
-    // _sprite->fillRect(x2, 0,  x2 - 36, 12, TFT_WHITE);
-    // _sprite->fillRect(0,  y2, x2 - 36, 12, TFT_WHITE);
-    // _sprite->fillRect(x2, y2, x2 - 36, 12, TFT_WHITE);
+    // getSetting()->getSprite()->fillSmoothRoundRect(5, 5, 112, 125, 15, backColor);
+    // getSetting()->getSprite()->fillSmoothRoundRect(123, 5, 112, 125, 15, backColor);
 
     getSetting()->getSprite()->unloadFont();
 }
