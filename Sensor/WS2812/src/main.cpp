597be793ba04d26925e00b41263a950ec0d6906e
Ilya Kozin <ikozin@yandex.ru>
2026-05-15 23:38:00 +0300

    update

---
+++ Sensor/WS2812/src/main.cpp
@@ -0,0 +1,27 @@
+#include <FastLED.h>
+#define NUM_LEDS 8
+CRGB leds[NUM_LEDS];
+ 
+void setup() { 
+  FastLED.addLeds<WS2812, 8>(leds, NUM_LEDS); 
+}
+ 
+void loop() {
+  leds[0] = CRGB::Red; 
+  leds[1] = CRGB::Green;
+  leds[2] = CRGB::Blue;
+  FastLED.show();
+  delay(500);
+
+  leds[0] = CRGB::Green; 
+  leds[1] = CRGB::Blue;
+  leds[2] = CRGB::Red;
+  FastLED.show();
+  delay(500);
+
+  leds[0] = CRGB::Blue; 
+  leds[1] = CRGB::Red;
+  leds[2] = CRGB::Green;
+  FastLED.show();
+  delay(500);
+}
