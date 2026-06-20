d0edf190482360c27cd219cce233ed3caef91182
Ilya Kozin <ikozin@yandex.ru>
2026-06-20 18:16:21 +0300

    update

--- Radio/help/Button/src/main.cpp
+++ Radio/help/Button/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
-uint8_t pins[] = { 8, 9, 10, 11, 12, 13 };
-volatile uint8_t values[sizeof(pins)] = { 0, 0, 0, 0, 0, 0 };
+uint8_t pins[] = { 8, 9, 10, 11, 12 };
+volatile uint8_t values[sizeof(pins)] = { 0, 0, 0, 0, 0 };
 
 //typedef void (*isr_handler_t)(uint8_t, uint8_t);
 
@@ -12,28 +12,24 @@ ISR (PCINT0_vect) {
     uint8_t changedbits = value ^ oldPINB;
     oldPINB = value;
 
-    if (changedbits & (1 << PB0)) { // Изменился D8
-        values[PB0] = value & (1 << PB0);
+    if (changedbits & bit(PB0)) { // Изменился D8
+        values[PB0] = value & bit(PB0);
     }
 
-    if (changedbits & (1 << PB1)) { // Изменился D9
-        values[PB1] = value & (1 << PB1);
+    if (changedbits & bit(PB1)) { // Изменился D9
+        values[PB1] = value & bit(PB1);
     }
 
-    if (changedbits & (1 << PB2)) { // Изменился D10
-        values[PB2] = value & (1 << PB2);
+    if (changedbits & bit(PB2)) { // Изменился D10
+        values[PB2] = value & bit(PB2);
     }
 
-    if (changedbits & (1 << PB3)) { // Изменился D11
-        values[PB3] = value & (1 << PB3);
+    if (changedbits & bit(PB3)) { // Изменился D11
+        values[PB3] = value & bit(PB3);
     }
 
-    if (changedbits & (1 << PB4)) { // Изменился D12
-        values[PB4] = value & (1 << PB4);
-    }
-
-    if (changedbits & (1 << PB5)) { // Изменился D13
-        values[PB5] = value & (1 << PB5);
+    if (changedbits & bit(PB4)) { // Изменился D12
+        values[PB4] = value & bit(PB4);
     }
 }
 
@@ -42,18 +38,22 @@ void setup() {
     Serial.begin(115200);
     Serial.println(F("Initialize Button"));
 
-    PCMSK0 |= 31;
-    for (uint8_t i = 0; i < sizeof(pins); i++) {
-        *digitalPinToPCMSK(pins[i]) |= bit (digitalPinToPCMSKbit(pins[i])); // Разрешаем PCINT для указанного пина
-        PCIFR  |= bit (digitalPinToPCICRbit(pins[i]));  // Очищаем признак запроса прерывания для соответствующей группы пинов
-        PCICR  |= bit (digitalPinToPCICRbit(pins[i]));  // Разрешаем PCINT для соответствующей группы пинов
-        //pinMode(pins[i], INPUT);
-    }
+    // Разрешаем PCINT для указанных пинов
+    PCMSK0 |= bit(PB0) | bit(PB1) | bit(PB2) | bit(PB3) | bit(PB4);
+    // Очищаем признак запроса прерывания для соответствующей группы пинов
+    PCIFR  |= 1;
+    // Разрешаем PCINT для соответствующей группы пинов
+    PCICR  |= 1;
+    // for (uint8_t i = 0; i < sizeof(pins); i++) {
+    //     *digitalPinToPCMSK(pins[i]) |= bit (digitalPinToPCMSKbit(pins[i])); // Разрешаем PCINT для указанного пина
+    //     PCIFR  |= bit (digitalPinToPCICRbit(pins[i]));  // Очищаем признак запроса прерывания для соответствующей группы пинов
+    //     PCICR  |= bit (digitalPinToPCICRbit(pins[i]));  // Разрешаем PCINT для соответствующей группы пинов
+    // }
 
 }
 
 void loop() {
-    delay(500);
+    delay(100);
     for (uint8_t i = 0; i < sizeof(pins); i++) {
         Serial.print(pins[i]);    
         Serial.print("(");    
